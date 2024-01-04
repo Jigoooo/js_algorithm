398596b6d2132d5a16a69e7d95b545f580b6d27b
jwkim <jwkim@triphos.co.kr>
2024-01-04 18:00:34 +0900

    first push

--- src/index.js
+++ src/index.js
@@ -11,3 +11,17 @@ readline.on('line', function(line) {
 
     process.exit();
 });
+
+
+const myObj = {
+    myMethod: function(params) {
+        // 뭔가 하기...
+    },
+
+    // 이렇게도  동작해요
+    myOtherMethod(params) {
+        console.log(params);
+    }
+};
+
+myObj.myOtherMethod('123')
