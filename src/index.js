c52d87689afdc5ea42396e314279b672ff322d92
jwkim <jwkim@triphos.co.kr>
2024-01-05 17:02:43 +0900

    first push

--- src/index.js
+++ src/index.js
@@ -13,15 +13,43 @@ readline.on('line', function(line) {
 });
 
 
-const myObj = {
-    myMethod: function(params) {
-        // 뭔가 하기...
+var o;
+
+// 프로토타입이 null인 객체 생성
+o = Object.create(null);
+
+o = {};
+// 위는 아래와 같습니다:
+o = Object.create(Object.prototype);
+
+// 샘플 속성 두개를 갖는 객체를 만드는 예.
+// (두 번째 매개변수는 키를 *속성 설명자*에 맵핑함을 주의하세요.)
+o = Object.create(Object.prototype, {
+    // foo는 정규 '값 속성'
+    foo: { writable: true, configurable: true, value: "hello" },
+    // bar는 접근자(accessor, getter-및-setter) 속성
+    bar: {
+        configurable: false,
+        get: function () {
+            return 10;
+        },
+        set: function (value) {
+            console.log("Setting `o.bar` to", value);
+        },
+        /* ES5 접근자로 코드는 이렇게 할 수 있습니다
+        get function() { return 10; },
+        set function(value) { console.log('setting `o.bar` to', value); } */
     },
+});
 
-    // 이렇게도  동작해요
-    myOtherMethod(params) {
-        console.log(params);
+
+class Search1 {
+    constructor(value) {
+        this.value = value;
     }
-};
+    [Symbol.search](string) {
+        return string.indexOf(this.value);
+    }
+}
 
-myObj.myOtherMethod('123')
+console.log('foobar'.search(new Search1('bar')));
